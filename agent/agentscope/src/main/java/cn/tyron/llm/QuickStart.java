65132b4c33e9367bdd8e845658c6573c24d0c716
zetian <czt1046@126.com>
2026-04-14 10:19:13 +0800

    feat：change name

--- agent/agentscope/src/main/java/cn/tyron/llm/QuickStart.java
+++ agent/agentscope/src/main/java/cn/tyron/llm/QuickStart.java
@@ -11,7 +11,7 @@ import java.awt.*;
 
 /**
  * @description: 创建一个智能体对象
- * @author: chenzt
+ * @author: tyron
  * @create: 2026-03-27
  **/
 public class QuickStart {
@@ -21,13 +21,29 @@ public class QuickStart {
         toolkit.registerTool(new SimpleTools());
 
         // 创建智能体
+//        ReActAgent jarvis = ReActAgent.builder()
+//                .name("Jarvis")
+//                .sysPrompt("你是一个名为 Jarvis 的助手")
+//                .model(OpenAIChatModel.builder()
+//                                .baseUrl("https://api.minimaxi.com/v1")
+//                .apiKey("sk-cp-TnaY0SNRAGgiFqFKQL5VGzfTEQHNFE92j-z2gsMxaWaIGQMRf3GP9_XxNmFvI-JVVe3dbm94-VxYLM7C8oOD3du-iErD75RNMoeOC5uGGQAdinJv6MbKJys")
+//                .modelName("MiniMax-M2.7")
+//                        .baseUrl("https://coding.dashscope.aliyuncs.com/v1")
+//                .apiKey("sk-sp-0bfe3b3113e548e5bbd67aea5e82e718")
+//                .modelName("qwen3.5-plus")
+//                                .baseUrl("https://coding.dashscope.aliyuncs.com/v1")
+//                .apiKey("sk-sp-3faf8233f00541abb638c2db209f9c94")
+//                .modelName("qwen3.5-plus")
+//                        .build())
+//                .toolkit(toolkit)
+//                .build();
         ReActAgent jarvis = ReActAgent.builder()
                 .name("Jarvis")
                 .sysPrompt("你是一个名为 Jarvis 的助手")
                 .model(OpenAIChatModel.builder()
-                        .baseUrl("https://api.aigcly.top/v1")
-                        .apiKey("sk-KNXGOMFJXgeqrd4VHLbZ86oNkHZS5hTFMNo0yTXk5gLaNAVw")
-                        .modelName("gpt-5.4")
+                        .baseUrl("https://aihubmix.com/v1")
+                        .apiKey("sk-4VXMEKJa8pJytWKJB301392dF88d42Bf8fFb6b91237cF184")
+                        .modelName("gpt-4o-free")
                         .build())
                 .toolkit(toolkit)
                 .build();
